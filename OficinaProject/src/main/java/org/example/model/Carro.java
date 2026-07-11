fe4be6104721ba5fbb9f78db8d0186bea17ca2cd
Maycon <deepplayr@gmail.com>
2026-07-11 16:12:05 -0300

    criação de classes clientes e git ignore

---
+++ OficinaProject/src/main/java/org/example/model/Carro.java
@@ -0,0 +1,46 @@
+package org.example.model;
+
+public class Carro {
+    private String modelo;
+    private String marca;
+    private String placa;
+    private Cliente proprietario;
+
+    public Carro(String modelo, String marca, String placa, Cliente proprietario) {
+        this.modelo = modelo;
+        this.marca = marca;
+        this.placa = placa;
+        this.proprietario = proprietario;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+    public Cliente getProprietario() {
+        return proprietario;
+    }
+
+    public void setProprietario(Cliente proprietario) {
+        this.proprietario = proprietario;
+    }
+}
---
+++ OficinaProject/src/main/java/org/example/model/Cliente.java
@@ -0,0 +1,35 @@
+package org.example.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cliente {
+    private String nome;
+    private String cpf;
+    private List<Carro> carros = new ArrayList<>();
+
+    public Cliente(String nome, String cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public List<Carro> getCarros() {
+        return carros;
+    }
+}
